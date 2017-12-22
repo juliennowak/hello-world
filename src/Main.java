03b059b43ce3836080716134181a1f3afbd74f35
Julien Nowak <julien.nowak@telecomnancy.eu>
2017-12-22 14:35:39 +0100

    Ajout du main

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import com.sun.deploy.util.StringUtils;
+
+import java.text.Normalizer;
+
+public class Main {
+    public static void main(String args[]) {
+        String s = "Lé éèàîïùûkebabs";
+        System.out.println(s);
+    }
+}
